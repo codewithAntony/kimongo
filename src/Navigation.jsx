86b545a2bcc635a1f7b37380b013e37bddeba1d3
Antony Murithi <antonymurithi51@gmail.com>
2025-12-09 20:18:19 -0800

    my latest portfolio

---
+++ src/Navigation.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Navigation = () => {
+    return (
+        <div>
+            <div>
+                <div>
+                    <h2>Kimong'o</h2>
+                </div>
+                <div>
+                    <span>Home</span>
+                    <span>About</span>
+                    <span>Services</span>
+                    <span>Projects</span>
+                    <span>Contact</span>
+                </div>
+                <div>
+                    <button>Let's chat</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navigation;
