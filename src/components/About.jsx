9c4cfc99a7a66267200990bebdfb4d82aef9c871
kimongo <antonymurithi51@gmail.com>
2025-12-17 21:13:18 +0300

    updates in about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,18 +4,16 @@ import { div } from 'framer-motion/client';
 
 function About() {
     return (
-        <div className="bg-[#081427]">
-            <div id="about" className="mx-auto min-h-screen max-w-7xl px-4">
-                <div className="flex flex-col pt-8 items-center space-y-3 sm:pt-15">
-                    <h1 className="text-[#5CF3CF] text-2xl uppercase sm:text-4xl">
-                        About Me
-                    </h1>
-                </div>
-                <div className="flex space-x-2 justify-around pt-[20px] sm:pt-[70px]">
-                    <div>
-                        <AboutTabs />
-                    </div>
-                </div>
+        <div className="bg-[#081427] min-h-screen flex items-center justify-center">
+            <div
+                id="about"
+                className="w-full max-w-7xl px-4 flex flex-col items-center"
+            >
+                <h1 className="text-[#5CF3CF] text-2xl uppercase sm:text-4xl mb-10">
+                    About Me
+                </h1>
+
+                <AboutTabs />
             </div>
         </div>
     );
